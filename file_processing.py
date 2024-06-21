68b3bb5e21059664933195ebd1bf48bf12825358
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-06-21 17:14:49 +0300

    Update file_processing.py

--- file_processing.py
+++ file_processing.py
@@ -3,8 +3,7 @@ import ffmpeg
 import logging
 from moviepy.editor import VideoFileClip, AudioFileClip
 import numpy as np
-from scipy.io import wavfile
-from config import audio_formats, video_formats
+from config import audio_formats, video_formats, trim_audio_to_10_minutes
 
 # Инициализация логгера
 logger = logging.getLogger(__name__)
@@ -17,29 +16,11 @@ def is_video_file(file_path):
     logger.info(f"Checking video file format for: {file_path}")
     return file_path.lower().endswith(tuple(video_formats))
 
-def generate_waveform(file_path, output_path='waveform.dat'):
+def generate_waveform():
     try:
-        wav_path = 'temp.wav'
-        ffmpeg.input(file_path).output(wav_path).run(overwrite_output=True)
-
-        samplerate, data = wavfile.read(wav_path)
-        if len(data.shape) > 1:
-            data = np.mean(data, axis=1)
-
-        max_value = np.max(np.abs(data))
-        if max_value > 0:
-            data = data / max_value
-
-        waveform = (data * 127 + 128).astype(np.uint8)
-        waveform = waveform[:960]
-        waveform_bytes = waveform.tobytes()
-
-        with open(output_path, 'wb') as f:
-            f.write(waveform_bytes)
-
-        os.remove(wav_path)
-
-        return waveform_bytes
+        # Генерация случайного waveform
+        waveform = np.random.randint(0, 256, size=960, dtype=np.uint8)
+        return waveform.tobytes()
     except Exception as e:
         logger.error(f"Error generating waveform: {e}")
         return None
@@ -48,8 +29,13 @@ def convert_to_voice(file_path):
     output_path = 'converted_voice.ogg'
     audio = AudioFileClip(file_path)
 
-    if audio.duration > 600:
+    # Проверяем, нужно ли обрезать аудио
+    if trim_audio_to_10_minutes and audio.duration > 600:
         audio = audio.subclip(0, 600)
+        temp_trimmed_path = "temp_trimmed_audio.wav"
+        audio.write_audiofile(temp_trimmed_path, codec='pcm_s16le')
+        file_path = temp_trimmed_path
+        audio = AudioFileClip(file_path)
 
     duration = int(audio.duration)
 
@@ -83,7 +69,11 @@ def convert_to_voice(file_path):
 
     audio_output.run(overwrite_output=True)
 
-    waveform = generate_waveform(output_path) if os.path.getsize(output_path) > 1 * 1024 * 1024 or audio.duration > 120 else None
+    # Генерация случайного waveform для длинных файлов
+    waveform = generate_waveform() if os.path.getsize(output_path) > 1 * 1024 * 1024 or audio.duration > 120 else None
+
+    if trim_audio_to_10_minutes and os.path.exists(temp_trimmed_path):
+        os.remove(temp_trimmed_path)
 
     return output_path, waveform, duration
 
@@ -111,4 +101,3 @@ def cleanup_files():
     for file in os.listdir('.'):
         if file.startswith('downloaded_media'):
             os.remove(file)
-
