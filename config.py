417193203486b92cb5a4e8f26e0dde6aa92b3f4f
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-06-23 13:36:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,59 +1,43 @@
 # Your API ID from my.telegram.org
-# Ваш API ID с my.telegram.org
-# 您从 my.telegram.org 获取的 API ID
 api_id = 'YOUR_API_ID'
 
 # Your API Hash from my.telegram.org
-# Ваш API Hash с my.telegram.org
-# 您从 my.telegram.org 获取的 API Hash
 api_hash = 'YOUR_API_HASH'
 
-# BotFather token (fill in if needed)
-# Токен BotFather(заполните, если необходимо)
-# BotFather 令牌，如果需要的话
+# BotFather token (fill in if you need public bot too)
 botfather_token = 'YOUR_BOTFATHER_TOKEN'
 
-# Add random waveform to voice messages
-# Добавить случайную спектрограмму аудиодорожки к голосовым сообщениям
-# 如果需要为音频文件添加随机波形图
+# Add random waveform to audio files
 add_random_waveform = True
 #add_random_waveform = False
 
-# Your Telegram user ID and allowed user IDs (delete others if no need)
-# Ваш Telegram user ID и разрешенные user ID (сотрите лишние, если не нужны)
-# 您的 Telegram 用户 ID 和允许的用户 ID（如果不需要，请删除其他的）
+# Allow all users to interact with the bot
+allow_all_users = True
+#allow_all_users = False
+
+# Your Telegram user ID and allowed user IDs (delete others if not needed)
 allowed_user_id = ['YOUR_USER_ID', 'USER_ID2', 'USER_ID3']
 
 # Language setting: 'RU' for Russian, 'EN' for English, 'CN' for Chinese
-# Настройка языка: 'RU' для русского, 'EN' для английского, 'CN' для китайского
-# 语言设置：'RU' 代表俄语，'EN' 代表英语，'CN' 代表中文
 #language = 'RU'
 language = 'EN'
 #language = 'CN'
 
 # Maximum file size in megabytes
-# Максимальный размер файла в мегабайтах
-# 最大文件大小（以兆字节为单位）
 max_file_size_mb = 100
 
 # Supported audio formats
-# Поддерживаемые аудио форматы
-# 支持的音频格式
 audio_formats = [
-    '.mp3', '.wav', '.ogg', '.oga', '.m4a', '.aac', '.flac', '.alac',
-    '.wma', '.aiff', '.opus', '.amr', '.mka'
+'.mp3', '.wav', '.ogg', '.oga', '.m4a', '.aac', '.flac', '.alac',
+'.wma', '.aiff', '.opus', '.amr', '.mka'
 ]
 
 # Supported video formats
-# Поддерживаемые видео форматы
-# 支持的视频格式
 video_formats = [
-    '.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.m4v',
-    '.mpg', '.mpeg', '.3gp', '.3g2', '.mxf', '.ogv', '.mts', '.m2ts'
+'.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm', '.m4v',
+'.mpg', '.mpeg', '.3gp', '.3g2', '.mxf', '.ogv', '.mts', '.m2ts'
 ]
 
 # Trim audio to 10 minutes if it's longer
-# Обрезать аудио до 10 минут, если оно дольше
-# 如果音频超过10分钟，则将其修剪到10分钟
 trim_audio_to_10_minutes = True
 #trim_audio_to_10_minutes = False
