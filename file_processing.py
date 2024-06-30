6f253a578a3f6dd8a79a8f1b796822ee6909f786
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-06-30 17:43:15 +0300

    Update file_processing.py

--- file_processing.py
+++ file_processing.py
@@ -1,169 +1,131 @@
-import logging
 import os
-import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-from aiogram.types import ContentType, InputFile, BotCommand
-from config import botfather_token, language, add_random_waveform, allow_all_users, allowed_user_id, max_file_size_mb, audio_formats, video_formats
-from messages import get_message
-from file_processing import is_audio_file, is_video_file, convert_to_voice, convert_to_round_video, cleanup_files, split_audio_file
-from telethon import TelegramClient
-from telethon.tl.types import DocumentAttributeAudio
+import ffmpeg
+import logging
+from moviepy.editor import VideoFileClip, AudioFileClip
+import numpy as np
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Initialize aiogram bot and dispatcher
-bot = Bot(token=botfather_token)
-dp = Dispatcher(bot)
+def is_audio_file(file_path):
+    try:
+        probe = ffmpeg.probe(file_path)
+        for stream in probe['streams']:
+            if stream['codec_type'] == 'audio' and 'video' not in [s['codec_type'] for s in probe['streams']]:
+                return True
+        return False
+    except ffmpeg.Error as e:
+        logger.error(f"ffmpeg error: {e}")
+        return False
 
-# Initialize telethon client with bot token
-session_name = 'bot_session'
-telethon_client = TelegramClient(session_name, api_id, api_hash)
-
-async def start_telethon_client():
-    await telethon_client.start(bot_token=botfather_token)
-
-asyncio.get_event_loop().run_until_complete(start_telethon_client())
-
-user_file_path = None
-service_message_ids = []
-media_message_ids = []
-bot_active = False  # Глобальная переменная для отслеживания состояния бота
+def is_video_file(file_path):
+    try:
+        probe = ffmpeg.probe(file_path)
+        for stream in probe['streams']:
+            if stream['codec_type'] == 'video':
+                return True
+        return False
+    except ffmpeg.Error as e:
+        logger.error(f"ffmpeg error: {e}")
+        return False
 
 def generate_waveform():
-    # Генерация случайной waveform
     waveform = np.random.randint(0, 256, size=80, dtype=np.uint8)
     return waveform.tobytes()
 
-async def set_commands():
-    commands = [
-        BotCommand(command="/start_voice_video_bot", description=get_message("command_start_description", language)),
-        BotCommand(command="/stop_voice_video_bot", description=get_message("command_stop_description", language))
-    ]
-    await bot.set_my_commands(commands)
+def convert_to_voice(file_path):
+    output_path = 'converted_voice.ogg'
+    audio = AudioFileClip(file_path)
+    duration = int(audio.duration)
 
-@dp.message_handler(commands=['start_voice_video_bot'])
-async def start(message: types.Message):
-    global user_file_path, bot_active
-    if not allow_all_users and str(message.from_user.id) not in allowed_user_id:
-        return
+    probe = ffmpeg.probe(file_path)
+    metadata = {}
+    for stream in probe['streams']:
+        if stream['codec_type'] == 'audio':
+            metadata.update(stream.get('tags', {}))
 
-    bot_active = True  # Включаем бота
-    user_file_path = None
-    msg1 = await message.reply(get_message("start", language))
-    msg2 = await message.reply(get_message("send_file", language))
-    service_message_ids.extend([msg1.message_id, msg2.message_id])
+    metadata.update({
+        'artist': 'Telegram',
+        'title': 'Telegram Voice Message',
+        'album': 'Telegram Voice Messages',
+        'encoder': 'Lavf58.76.100'
+    })
 
-@dp.message_handler(commands=['stop_voice_video_bot'])
-async def stop(message: types.Message):
-    global user_file_path, bot_active
-    if not allow_all_users and str(message.from_user.id) not in allowed_user_id:
-        return
+    input_file = ffmpeg.input(file_path)
+    audio_output = ffmpeg.output(input_file, output_path,
+                                 acodec='libopus',
+                                 audio_bitrate='64k',
+                                 format='ogg',
+                                 application='voip',
+                                 compression_level=10,
+                                 ar='48000',
+                                 ac=1)
 
-    bot_active = False  # Останавливаем бота
-    user_file_path = None
-    msg = await message.reply(get_message("stop", language))
-    service_message_ids.append(msg.message_id)
-    
-    for msg_id in service_message_ids:
-        try:
-            await bot.delete_message(message.chat.id, msg_id)
-        except Exception as e:
-            logging.error(f"Error deleting message {msg_id}: {e}")
-    service_message_ids.clear()
-    cleanup_files()
+    for key, value in metadata.items():
+        audio_output = audio_output.global_args('-metadata', f'{key}={value}')
 
-@dp.message_handler(content_types=[ContentType.DOCUMENT, ContentType.VIDEO, ContentType.AUDIO])
-async def handle_media(message: types.Message):
-    global user_file_path, bot_active, service_message_ids
-    if not bot_active:
-        return  # Игнорируем сообщения, если бот не активен
+    audio_output = audio_output.global_args('-metadata', 'comment=Telegram Voice Message')
 
-    if not allow_all_users and str(message.from_user.id) not in allowed_user_id:
-        return
+    audio_output.run(overwrite_output=True)
 
-    file = await message.document.download(destination_dir='files') if message.document else await message.video.download(destination_dir='files') if message.video else await message.audio.download(destination_dir='files')
-    user_file_path = file.name
+    waveform = generate_waveform() if os.path.getsize(output_path) > 1 * 1024 * 1024 or audio.duration > 120 else None
 
-    # Проверка размера файла
-    if os.path.getsize(user_file_path) > max_file_size_mb * 1024 * 1024:
-        msg = await message.reply(get_message("large_file", language))
-        service_message_ids.append(msg.message_id)
-        os.remove(user_file_path)
-        return
+    return output_path, waveform, duration
 
-    # Проверка формата файла
-    file_ext = os.path.splitext(user_file_path)[1].lower()
-    if not (file_ext in audio_formats or file_ext in video_formats):
-        msg = await message.reply(get_message("invalid_format", language))
-        service_message_ids.append(msg.message_id)
-        os.remove(user_file_path)
-        return
+def convert_to_round_video(file_path):
+    clip = VideoFileClip(file_path)
 
-    msg = await message.reply(get_message("processing_download", language))
-    service_message_ids.append(msg.message_id)
-    
-    try:
-        if is_audio_file(user_file_path):
-            audio_parts = split_audio_file(user_file_path, chunk_length=600)
-            await bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text=get_message("processing_conversion", language))
-            for part in audio_parts:
-                output_file, waveform, duration = convert_to_voice(part)
-                if add_random_waveform:
-                    waveform_data = generate_waveform()
-                    async with telethon_client:
-                        await telethon_client.send_file(
-                            message.chat.id,
-                            file=output_file,
-                            voice_note=True,
-                            attributes=[
-                                DocumentAttributeAudio(
-                                    duration=duration,
-                                    voice=True,
-                                    waveform=waveform_data
-                                )
-                            ]
-                        )
-                else:
-                    media_msg = await bot.send_voice(
-                        chat_id=message.chat.id,
-                        voice=InputFile(output_file),
-                        duration=duration
-                    )
-                    media_message_ids.append(media_msg.message_id)
-                os.remove(output_file)  # Clean up the converted file
-        elif is_video_file(user_file_path):
-            video_parts = split_video_file(user_file_path, chunk_length=60)
-            await bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text=get_message("processing_conversion", language))
-            for part in video_parts:
-                output_file = convert_to_round_video(part)
-                media_msg = await bot.send_video_note(
-                    chat_id=message.chat.id,
-                    video_note=InputFile(output_file)
-                )
-                media_message_ids.append(media_msg.message_id)
-                os.remove(output_file)  # Clean up the converted file
-        else:
-            msg = await bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text=get_message("invalid_format", language))
-            service_message_ids.append(msg.message_id)
-            user_file_path = None
-            return
-        await bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text=get_message("processing_send", language))
-    except KeyError as e:
-        msg = await message.reply(f"Missing key in messages: {e}")
-        service_message_ids.append(msg.message_id)
-    except Exception as e:
-        msg = await message.reply(f"Error processing file: {e}")
-        service_message_ids.append(msg.message_id)
-        user_file_path = None
-        return
+    if clip.duration > 60:
+        clip = clip.subclip(0, 60)
 
-    msg = await message.reply(get_message("send_file", language))
-    service_message_ids.append(msg.message_id)
-    cleanup_files()
+    min_dimension = min(clip.size)
+    x_center = (clip.size[0] - min_dimension) // 2
+    y_center = (clip.size[1] - min_dimension) // 2
+    clip = clip.crop(x1=x_center, y1=y_center, x2=x_center + min_dimension, y2=y_center + min_dimension)
+    temp_file_path = "temp_square_video.mp4"
+    clip.write_videofile(temp_file_path, codec='libx264', audio_codec='aac')
+    output_path = 'converted_video.mp4'
+    ffmpeg.input(temp_file_path).output(output_path, vf='scale=320:320,format=yuv420p', vcodec='libx264', video_bitrate='800k').run(overwrite_output=True)
+    os.remove(temp_file_path)
+    return output_path
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(set_commands())
-    executor.start_polling(dp, skip_updates=True)
+def split_audio_file(file_path, chunk_length=600):
+    audio = AudioFileClip(file_path)
+    duration = audio.duration
+    parts = []
+    for i in range(0, int(duration), chunk_length):
+        part_path = f"{file_path}_part_{i // chunk_length}.ogg"
+        audio.subclip(i, min(i + chunk_length, duration)).write_audiofile(part_path, codec='libvorbis')
+        parts.append(part_path)
+    return parts
+
+def split_video_file(file_path, chunk_length=60):
+    video = VideoFileClip(file_path)
+    duration = video.duration
+    parts = []
+    for i in range(0, int(duration), chunk_length):
+        part_path = f"{file_path}_part_{i // chunk_length}.mp4"
+        video.subclip(i, min(i + chunk_length, duration)).write_videofile(part_path, codec='libx264', audio_codec='aac')
+        parts.append(part_path)
+    return parts
+
+def cleanup_files():
+    logger.info("Starting cleanup process")
+    # Удаление известных временных файлов
+    for file in ['converted_voice.ogg', 'converted_video.mp4', 'waveform.dat']:
+        if os.path.exists(file):
+            logger.info(f"Removing file: {file}")
+            os.remove(file)
+
+    # Удаление всех поддерживаемых аудио и видео файлов в директории files
+    supported_formats = [ext.lower() for ext in audio_formats + video_formats]
+    for root, dirs, files in os.walk('files'):
+        logger.info(f"Scanning directory: {root}")
+        for file in files:
+            logger.info(f"Found file: {file}")
+            if any(file.lower().endswith(ext) for ext in supported_formats):
+                file_path = os.path.join(root, file)
+                logger.info(f"Removing file: {file_path}")
+                try:
+                    os.remove(file_path)
+                except Exception as e:
+                    logger.error(f"Error removing file {file_path}: {e}")
