1b0fa0d3ca45888a13a5a09d18d764d566380c21
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-07-01 19:43:39 +0300

    Update file_processing.py

--- file_processing.py
+++ file_processing.py
@@ -94,6 +94,7 @@ def convert_to_round_video(file_path):
     os.remove(temp_file_path)
     return output_path
 
+
 def split_audio_file(file_path, chunk_length=600):
     audio = AudioFileClip(file_path)
     duration = int(audio.duration)
